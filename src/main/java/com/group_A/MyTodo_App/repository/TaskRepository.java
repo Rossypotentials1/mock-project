fa6ec00ac23a2ca5d4b028b017d993ad0ef87ba1
Deraclassic <chiderauzoigwe2@gmail.com>
2024-03-30 16:49:39 +0100

    view and edit todo

--- src/main/java/com/group_A/MyTodo_App/repository/TaskRepository.java
+++ src/main/java/com/group_A/MyTodo_App/repository/TaskRepository.java
@@ -3,5 +3,10 @@ package com.group_A.MyTodo_App.repository;
 import com.group_A.MyTodo_App.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId_Id(Long userId);
+
 }
+
--- src/main/java/com/group_A/MyTodo_App/service/TaskService.java
+++ src/main/java/com/group_A/MyTodo_App/service/TaskService.java
@@ -1,4 +1,19 @@
 package com.group_A.MyTodo_App.service;
 
+import com.group_A.MyTodo_App.dto.TaskDto;
+import com.group_A.MyTodo_App.entity.Task;
+import com.group_A.MyTodo_App.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public interface TaskService {
+
+    public List<Task> findTasksByUserId(Long UserId);
+    public Task updateTask(Long taskId, TaskDto taskUpdateDto);
 }
+
+
