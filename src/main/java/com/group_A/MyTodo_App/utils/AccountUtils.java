f6e69711b523cc445348a8a74f7eb354a43af265
rossypotentials <urokorosemarynnena@gmail.com>
2024-04-29 12:11:00 +0100

    account utils added

--- src/main/java/com/group_A/MyTodo_App/utils/AccountUtils.java
+++ src/main/java/com/group_A/MyTodo_App/utils/AccountUtils.java
@@ -5,6 +5,7 @@ public class AccountUtils {
   public final static String ACCOUNT_CREATION_MESSAGE = "Account has been created successfully ";
 
   public  final static String UNSUCCESSFUL = "Could not create account";
+  public  final static String UNSUCCESSFUL_CODE = "004";
   public final static String LOGIN_SUCCESS_CODE = "002";
   public final static String LOGIN_SUCCESS_MESSAGE = "login successfully ";
 
